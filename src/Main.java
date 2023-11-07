a7796a1b787e5cd33a124bfa1d395a4249ffccf0
LaisLatifi <mudaser1992@gmail.com>
2023-11-07 15:17:10 -0800

    last line

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is my second practice in this week!");
         System.out.println("kho and dega chtor asty");
+        System.out.println(" the last line for this code");
     }
 }
