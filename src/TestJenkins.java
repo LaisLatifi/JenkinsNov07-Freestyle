9fb28122a8961c8eb562fe56037d3d8197bdd962
LaisLatifi <mudaser1992@gmail.com>
2023-11-09 08:39:08 -0800

    Test2 For jenkins

--- src/TestJenkins.java
+++ src/TestJenkins.java
@@ -1,5 +1,6 @@
 public class TestJenkins {
     public static void main(String[] args) {
         System.out.println("jenkins test");
+        System.out.println("lets try the new lessons");
     }
 }
